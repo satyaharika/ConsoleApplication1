7008aca51c2cb3e216972326f9f372e8e86f4a10
Administrator <Administrator@VSALM>
2016-06-28 19:55:42 -0700

    hhhh  gg

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -4,14 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace ConsoleApplication2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Pulling data");
+            fghghgnmujkmkmmk
         }
     }
 }
